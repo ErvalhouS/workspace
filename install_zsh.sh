3deeb0f2e08dab163c632ac477f84bba4b4c2d64
Fernando Bellincanta <ervalhous@hotmail.com>
2017-05-26 18:14:31 +0000

    Adding powerleel9k to zsh install script

--- install_zsh.sh
+++ install_zsh.sh
@@ -4,3 +4,4 @@ sudo apt-get install zsh
 sudo apt-get install git-core
 sudo curl -L http://install.ohmyz.sh | sh
 chsh -s $(which zsh) $(whoami)
+git clone https://github.com/bhilburn/powerlevel9k.git ~/powerlevel9k
